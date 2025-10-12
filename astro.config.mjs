6d31161dd4125d0630a5bf7dcb0964b0bf73c891
toko-ivt <kizawa@omiseweb.com>
2025-10-13 07:16:35 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,25 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sanity from '@sanity/astro';
 import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sanity({
+      projectId: '4q6dxeoe',
+      dataset: 'production',
+      useCdn: false,
+      studioBasePath: '/admin',
+    }),
+  ],
   vite: {
     resolve: {
       alias: {
         '~': fileURLToPath(new URL('./src', import.meta.url)),
-        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+        '@components': fileURLToPath(
+          new URL('./src/components', import.meta.url)
+        ),
         '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
         '@data': fileURLToPath(new URL('./src/data', import.meta.url)),
       },
