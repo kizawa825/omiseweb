593dfc94eb8e1666857cedb0ce1c85e2a13779d5
kizawa825 <kizawa@mihachi.com>
2025-10-14 20:58:34 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sanity from '@sanity/astro';
+// 'sanity' ではなく 'sanityIntegration' を{}で囲ってインポートします
+import { sanityIntegration } from '@sanity/astro';
 import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
   integrations: [
     tailwind(),
-    sanity({
-      // 環境変数から読み込むように変更
-      projectId: process.env.SANITY_PROJECT_ID,
-      dataset: process.env.SANITY_DATASET,
+    // こちらも 'sanity' から 'sanityIntegration' に変更します
+    sanityIntegration({
+      projectId: '4q6dxeoe',
+      dataset: 'production',
       useCdn: false,
       studioBasePath: '/admin',
     }),
   ],
-  vite: {
-    resolve: {
-      alias: {
-        '~': fileURLToPath(new URL('./src', import.meta.url)),
-        '@components': fileURLToPath(
-          new URL('./src/components', import.meta.url)
-        ),
-        '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
-        '@data': fileURLToPath(new URL('./src/data', import.meta.url)),
-      },
-    },
-  },
+  // ... (viteの設定などは変更なし)
 });
